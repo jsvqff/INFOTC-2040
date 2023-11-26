698e2d08c7f50ff4f39ed09c3c8c4989efe00e81
Jim Vespa <143173891+jsvqff@users.noreply.github.com>
2023-11-26 17:30:54 -0600

    Add files via upload

---
+++ Automobile/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+public enum AutoType
+{
+    Sedan,
+    Truck,
+    Van,
+    SUV
+}
+
+public class Automobile
+{
+    private string make;
+    private string model;
+    private string vin;
+    private string color;
+    private int year;
+    private AutoType type;
+
+    public Automobile(string make, string model, int year, string vin, string color, AutoType type)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.vin = vin;
+        this.color = color;
+        this.type = type;
+    }
+
+    public string getMake()
+    {
+        return make;
+    }
+
+    public string getModel()
+    {
+        return model;
+    }
+
+    public string getVin()
+    {
+        return vin;
+    }
+
+    public string getColor()
+    {
+        return color;
+    }
+
+    public void setColor(string newColor)
+    {
+        color = newColor;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+    public AutoType getType()
+    {
+        return type;
+    }
+
+    public int getAutoAge()
+    {
+        int currentYear = DateTime.Now.Year;
+        return currentYear - year;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nCreating the first Automobile\n---------------");
+        Automobile auto1 = new Automobile("Tesla", "Model X", 2020, "12345", "blue", AutoType.Sedan);
+        Console.WriteLine($"Make: {auto1.getMake()} \nModel: {auto1.getModel()}\nYear: {auto1.getYear()}\nType: {auto1.getType()} \nVIN: {auto1.getVin()}");
+        Console.WriteLine($"Color: {auto1.getColor()}");
+        Console.WriteLine("\nChanging the Colour\n---------------");
+        auto1.setColor("black");
+        Console.WriteLine($"Color: {auto1.getColor()}");
+
+        Console.WriteLine("\nCreating the second Automobile\n---------------");
+        Automobile auto2 = new Automobile("Mercedes", "G-Wagon", 2017, "24578", "silver", AutoType.SUV);
+        Console.WriteLine($"Make: {auto2.getMake()}\nModel: {auto2.getModel()}\nYear: {auto2.getYear()}\nType: {auto2.getType()}\nVIN: {auto2.getVin()}");
+
+        Console.WriteLine("\nPrinting Automobile Ages\n---------------");
+        Console.WriteLine($"Auto1 Age: {auto1.getAutoAge()} years");
+        Console.WriteLine($"Auto2 Age: {auto2.getAutoAge()} years");
+    }
+}
+
---
+++ C# Basics/CsharpBasics.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+
+const byte sample1 = 0x3A;
+byte sample2 = 58;
+int heartRate = 85;
+double deposits = 135002796;
+const float acceleration = 9.800F;
+float mass = 14.6F;
+double distance = 129.763001;
+bool lost = true;
+bool expensive = true;
+int choice = 2;
+const char integral = '\u222B';
+const string greeting = "Hello";
+string name = "Karen";
+
+if (sample1 == sample2)
+{
+    Console.WriteLine("The samples are equal.");
+}
+else
+{
+    Console.WriteLine("The samples are not equal.");
+}
+
+if (heartRate >= 40 && heartRate <= 80)
+{
+    Console.WriteLine("Heart rate is normal.");
+}
+else
+{
+    Console.WriteLine("Heart rate is not normal.");
+}
+
+if (deposits >= 100000000)
+{
+    Console.WriteLine("You are exceedingly wealthy.");
+}
+else
+{
+    Console.WriteLine("Sorry you are so poor.");
+}
+
+float force = mass * acceleration;
+Console.WriteLine($"force = {force}");
+
+Console.WriteLine($"{distance} is the distance.");
+
+if (lost == true && expensive == true)
+{
+    Console.WriteLine("I am really sorry! I will get the manager.");
+}
+else if (lost == true && expensive == false)
+{
+    Console.WriteLine("Here is a coupon for 10% off.");
+}
+
+if (choice == 1)
+{
+    Console.WriteLine("You chose 1.");
+}
+else if (choice == 2)
+{
+    Console.WriteLine("You chose 2.");
+}
+else if (choice == 3)
+{
+    Console.WriteLine("You chose 3.");
+}
+else
+{
+    Console.WriteLine("You made an unknown choice.");
+}
+
+Console.WriteLine($"{integral} is an integral.");
+
+for (int i = 5; i < 11; i++)
+{
+    Console.WriteLine($"i = {i}");
+}
+
+int age = 0;
+while (age < 6)
+{
+    Console.WriteLine($"age = {age}");
+    age++;
+}
+
+Console.WriteLine($"{greeting} {name}!");
