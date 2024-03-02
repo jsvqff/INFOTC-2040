0ce452e7ba90ea2f102fa556a6ef2a4ce7392694
Jim Vespa <143173891+jsvqff@users.noreply.github.com>
2024-03-02 00:26:37 -0600

    Add files via upload

---
+++ Grade Converter/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------GRADE CONVERTER PROGRAM----------");
+            string firstName, lastName;
+            Console.Write("Enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine($"Hello, {firstName} {lastName}!");
+
+            while (true)
+            {
+                List<double> grades = GetGrades();
+                ConvertAndPrintGrades(grades);
+                PrintGradeStatistics(grades);
+
+                Console.Write("Do you have more grades to convert? (yes/no): ");
+                string response = Console.ReadLine().ToLower();
+                if (response != "yes")
+                    break;
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+
+        static List<double> GetGrades()
+        {
+            List<double> grades = new List<double>();
+            Console.Write("Enter the number of grades you need to convert: ");
+            if (int.TryParse(Console.ReadLine(), out int numberOfGrades))
+            {
+                for (int i = 1; i <= numberOfGrades; i++)
+                {
+                    double grade;
+                    bool validInput = false;
+                    do
+                    {
+                        Console.Write($"Enter grade {i}: ");
+                        string input = Console.ReadLine();
+                        if (double.TryParse(input, out grade))
+                        {
+                            validInput = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter a valid numeric grade.");
+                        }
+                    } while (!validInput);
+
+                    grades.Add(grade);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            return grades;
+        }
+
+        static char ConvertToLetterGrade(double grade)
+        {
+            if (grade >= 90)
+                return 'A';
+            else if (grade >= 80)
+                return 'B';
+            else if (grade >= 70)
+                return 'C';
+            else if (grade >= 60)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        static void ConvertAndPrintGrades(List<double> grades)
+        {
+            Console.WriteLine("Grade Conversion:");
+            Console.WriteLine("--------------------------");
+            foreach (var grade in grades)
+            {
+                char letterGrade = ConvertToLetterGrade(grade);
+                Console.WriteLine($"A score of {grade} is a {letterGrade} grade");
+            }
+        }
+
+        static void PrintGradeStatistics(List<double> grades)
+        {
+            Console.WriteLine("\nGrade Statistics");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine($"Number of grades: {grades.Count}");
+            double average = CalculateAverageGrade(grades);
+            Console.WriteLine($"Average Grade: {average} ==> {ConvertToLetterGrade(average)}");
+        }
+
+        static double CalculateAverageGrade(List<double> grades)
+        {
+            if (grades.Count == 0)
+                return 0;
+
+            double total = 0;
+            foreach (var grade in grades)
+            {
+                total += grade;
+            }
+            return total / grades.Count;
+        }
+    }
+}
---
+++ MealTime/mealTime-answer/mealTime/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace mealTime;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //prompt user what time is it
+        Console.WriteLine("What time is it? ");
+        //get user response
+        string userResponse = Console.ReadLine()!;
+        //send response to function
+        
+        float timeNumber = convertTime(userResponse);
+        
+        //use if statement to determine what time it is
+        if(timeNumber >= 7 && timeNumber <= 8){
+            Console.WriteLine("Breakfast Time!");
+        }else if(timeNumber >= 12 && timeNumber <= 13){
+            Console.WriteLine("Lunch Time!");
+        }else if(timeNumber >= 18 && timeNumber <= 19){
+            Console.WriteLine("Dinner Time!");
+        }else{
+            Console.WriteLine("Not time to eat!");
+        }
+    }
+
+    public static float convertTime(string time){
+        //in function, use split to get the hours and minutes
+        string []timeParts = time.Split(":");
+        //get hours and minutes
+        float hours = float.Parse(timeParts[0]);
+        float minutes = float.Parse(timeParts[1]);
+
+        //convert minutes to a decimal
+        minutes = minutes/60;
+        
+        return hours + minutes;
+    }
+}
+
+    
+
